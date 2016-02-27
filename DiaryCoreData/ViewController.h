7d7a8a1daea7e0e27c8a8e0e86e126c6991e70ea
alexhickman <alex.hickman3@gmail.com>
2016-02-26 23:51:34 -0800

    Initial Commit

---
+++ DiaryCoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DiaryCoreData
+//
+//  Created by Hickman on 2/26/16.
+//  Copyright © 2016 Hickman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
